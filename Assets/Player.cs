bfa07be6789429796d722f5c03ed420e37e4fa90
kurenai0413 <kurenai0413@hotmail.com>
2016-11-25 21:07:41 +0800

    Add project files.

---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float jump_speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetMouseButtonDown(0))
+        {
+            this.GetComponent<Rigidbody>().velocity = Vector3.up * this.jump_speed;
+
+            // this.gameObject;
+            // 說明表示: the game object this componenet is attached to.
+        }
+	}
+}
